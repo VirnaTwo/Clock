1c069cb12c9b9271087bbddb75573844b8f801ec
VirnaTwo <Oyapoxkanee@protonmail.com>
2022-01-24 22:56:40 +0100

    master

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ async function Horloge() {
 
 	while (true) {
 		time.textContent = `${new Date().getHours()}h : ${new Date().getMinutes()}m : ${new Date().getSeconds()}s`
-		date.textContent = `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
-		document.body.style = `background-color: rgb(${new Date().getHours()/24*255},
-													${new Date().getMinutes()/60*255},
-													${new Date().getSeconds()/60*255})`
+		date.textContent = `${new Date().getDate()}/${new Date().getDay()}/${new Date().getFullYear()}`
+		
+		document.getElementById('nth-child1').style = `stroke-dashoffset: ${(100 * 6) - ((100 * 6) * (new Date().getSeconds()/60*100)) / 100}`
+		document.getElementById('nth-child2').style = `stroke-dashoffset: ${(100 * 6) - ((100 * 6) * (new Date().getSeconds()/60*100)) / 100}`
 
+		// document.body.style = `background-color: rgb(${new Date().getHours()/24*255}, ${new Date().getMinutes()/60*255}, ${new Date().getSeconds()/60*255})`
 		await wait_s(1000)
 	}
 }
 
 Horloge()
+
