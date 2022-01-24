3082d09f0c115f3415130c9b09550069652072b0
VirnaTwo <Oyapoxkanee@protonmail.com>
2022-01-24 18:02:50 +0100

    master

---
+++ script.js
@@ -0,0 +1,19 @@
+const wait_s = async ms => new Promise(done => setTimeout(done, ms));
+
+
+async function Horloge() {
+    const time = document.getElementById('time')
+    const date = document.getElementById('date')
+
+	while (true) {
+		time.textContent = `${new Date().getHours()}h : ${new Date().getMinutes()}m : ${new Date().getSeconds()}s`
+		date.textContent = `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
+		document.body.style = `background-color: rgb(${new Date().getHours()/24*255},
+													${new Date().getMinutes()/60*255},
+													${new Date().getSeconds()/60*255})`
+
+		await wait_s(1000)
+	}
+}
+
+Horloge()
