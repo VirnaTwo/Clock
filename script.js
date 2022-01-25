0c8d36ea542cd7b0c9b614b8203861dac760a925
VirnaTwo <Oyapoxkanee@protonmail.com>
2022-01-26 00:38:47 +0100

    master

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function Horloge() {
 		document.getElementsByTagName('text')[1].textContent = `${new Date().getMinutes()}m`
 
 		document.getElementsByTagName('circle')[5].style.strokeDashoffset = window.innerWidth/2.1223-new Date().getSeconds()/60*window.innerWidth/2.1223
-		document.getElementsByTagName('circle')[5].style.stroke = `rgb(${new Date().getSeconds()/60*255}, ${new Date().getMinutes()/60*255}, ${new Date().getHours()/24*255})`
+		document.getElementsByTagName('circle')[5].style.stroke = `rgb(${new Date().getSeconds()/60*255}, ${new Date().getHours()/24*255}, ${new Date().getMinutes()/60*255})`
 		document.getElementsByTagName('text')[2].textContent = `${new Date().getSeconds()}s`
 
 		// document.body.style = `background-color: rgb(${new Date().getHours()/24*255}, ${new Date().getMinutes()/60*255}, ${new Date().getSeconds()/60*255})`
