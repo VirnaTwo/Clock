acf1ed8160e0f01b11304d3311035292c28a724a
VirnaTwo <Oyapoxkanee@protonmail.com>
2022-01-25 15:39:45 +0100

    master

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ async function Horloge() {
 	while (true) {
 		time.textContent = `${new Date().getHours()}h : ${new Date().getMinutes()}m : ${new Date().getSeconds()}s`
 		date.textContent = `${new Date().getDate()}/${new Date().getDay()}/${new Date().getFullYear()}`
-		
-		document.getElementById('nth-child1').style = `stroke-dashoffset: ${(100 * 6) - ((100 * 6) * (new Date().getSeconds()/60*100)) / 100}`
-		document.getElementById('nth-child2').style = `stroke-dashoffset: ${(100 * 6) - ((100 * 6) * (new Date().getSeconds()/60*100)) / 100}`
 
-		// document.body.style = `background-color: rgb(${new Date().getHours()/24*255}, ${new Date().getMinutes()/60*255}, ${new Date().getSeconds()/60*255})`
+		document.getElementsByTagName('circle')[1].style.strokeDashoffset = 440-new Date().getHours()/59*440
+		document.getElementsByTagName('text')[0].textContent = `${new Date().getHours()} Hours`
+
+		document.getElementsByTagName('circle')[3].style.strokeDashoffset = 440-new Date().getMinutes()/59*440
+		document.getElementsByTagName('text')[1].textContent = `${new Date().getMinutes()} Minutes`
+
+		document.getElementsByTagName('circle')[5].style.strokeDashoffset = 440-new Date().getSeconds()/59*440
+		document.getElementsByTagName('text')[2].textContent = `${new Date().getSeconds()} Seconds`
+
+		//document.body.style = `background-color: rgb(${new Date().getHours()/24*255}, ${new Date().getMinutes()/60*255}, ${new Date().getSeconds()/60*255})`
 		await wait_s(1000)
 	}
 }
